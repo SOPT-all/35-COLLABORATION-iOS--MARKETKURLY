2b92454e3bb999888662422f7632653236fd23fc
sangwook <operawook@gmail.com>
2024-11-28 01:15:25 +0900

    [feat] #2 review data model

---
+++ MarketKurly/Presentation/Review/Model/ReviewData.swift
@@ -0,0 +1,18 @@
+//
+//  ReviewData.swift
+//  MarketKurly
+//
+//  Created by 우상욱 on 11/22/24.
+//
+
+import Foundation
+
+struct ReviewData {
+    let isAdvertisement: Bool
+    let name: String
+    let discription: String
+    let imageUrls: [String]
+    let rating: Double
+    let review: String
+    let date: String
+}
